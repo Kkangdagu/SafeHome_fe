68322fc2e398d7078e0b115039a94ffbb3934f16
OH <rudals2148@naver.com>
2024-02-21 12:07:45 +0900

    [✨feat]: 페이지 생성

---
+++ app/analysis/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from '@/components/Layout';
+
+export default function AnalysisPage() {
+  return (
+    <div className="w-full max-w-[690px] h-full">
+      <Header />
+    </div>
+  );
+}
