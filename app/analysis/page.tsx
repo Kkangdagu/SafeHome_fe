877395c7a7dfb36371771563cd2f36fb8bf87823
OH <rudals2148@naver.com>
2024-02-24 20:51:14 +0900

    [✨feat]: 분석 페이지 생성

---
+++ app/analysis/page.tsx
@@ -0,0 +1,13 @@
+import { UploadForm } from '@/components/Analysis';
+import { Header, SubContainer } from '@/components/Layout';
+
+export default function Analysis() {
+  return (
+    <>
+      <Header />
+      <SubContainer>
+        <UploadForm />
+      </SubContainer>
+    </>
+  );
+}
