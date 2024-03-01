5d8dff99500feea921c0f56cf857ed12f518cbd2
OH <rudals2148@naver.com>
2024-03-01 19:16:23 +0900

    [✨feat]: 메인 화면 관련 컴포넌트 및 페이지

---
+++ components/Common/Banner/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import 'swiper/css';
+import './banner.styles.css';
+
+import Image from 'next/image';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { IHomePresenter } from '@/components/home/Home.types';
+
+export default function Banner({ onBoarding }: IHomePresenter) {
+  return (
+    <Swiper
+      pagination={{ type: 'fraction' }}
+      modules={[Pagination]}
+      loop
+      className="h-[160px] rounded-md my-12">
+      {onBoarding.body.map((v) => (
+        <SwiperSlide key={v.lastChngRegDttm}>
+          <Image
+            src={v.thumbnailImgUrl}
+            alt="정책 이미지"
+            width={358}
+            height={160}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
