2346a0fca732f5efc73a9f6c128b26549ecb722e
OH <rudals2148@naver.com>
2024-03-03 20:11:38 +0900

    [✨feat]: 공통 컴포넌트

---
+++ components/Common/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full h-[98px] -mx-4 absolute bottom-0 bg-[#6C9BF2] flex justify-center">
+      <span className="text-[13px] text-white-0 mt-5">
+        개인정보처리방침ㅣ이용약관ㅣ고객문의
+      </span>
+    </footer>
+  );
+}
