a2a5a601e82f937bd37f2d7484c2b241c0af3e97
OH <rudals2148@naver.com>
2024-03-17 18:21:00 +0900

    [✨feat]: 토근 재발급 요청

--- utils/intercepter.ts
+++ utils/intercepter.ts
@@ -15,4 +15,39 @@ instance.interceptors.request.use(function axiosIntercepter(config) {
   return copyConfig;
 });
 
+instance.interceptors.response.use(
+  function responseFn(response) {
+    return response;
+  },
+  async function errorFn(error) {
+    const requestRetry = error.config;
+    // eslint-disable-next-line no-underscore-dangle
+    if (error.response.status === 401) {
+      // eslint-disable-next-line no-underscore-dangle
+      requestRetry._retry = true;
+      const refreshToken = localStorage.getItem('refresh-token');
+      const accessToken = localStorage.getItem('access-token');
+      return (
+        axios
+          .post(`${process.env.NEXT_PUBLIC_BASE_URL}/reissue`, {
+            accessToken,
+            refreshToken,
+          })
+          // eslint-disable-next-line consistent-return
+          .then((res) => {
+            if (res.status === 200) {
+              localStorage.setItem('access-token', res.data.accessToken);
+              requestRetry.headers.Authorization = `Bearer ${res.data.accessToken}`;
+              return instance(requestRetry);
+            }
+          })
+          .catch((err) => {
+            return err;
+          })
+      );
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default instance;
